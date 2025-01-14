ae6c8271c2272400b9760f3e4dd12c0de3a3d0be
ClaraBH <144669684+ClaraBH@users.noreply.github.com>
2025-01-14 16:05:24 +0000

    neo4j python file

--- export-neo4j.py
+++ export-neo4j.py
@@ -32,9 +32,9 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
 
     # Films
     exportedCount = 0
-    cursor.execute("SELECT COUNT(1) FROM TFilm")
+    cursor.execute("SELECT COUNT(1) FROM tFilm")
     totalCount = cursor.fetchval()
-    cursor.execute("SELECT idFilm, primaryTitle, startYear FROM TFilm")
+    cursor.execute("SELECT idFilm, primaryTitle, startYear FROM tFilm")
     while True:
         importData = []
         rows = cursor.fetchmany(BATCH_SIZE)
@@ -42,9 +42,10 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
             break
 
         i = 0
+        print(rows)
         for row in rows:
             # Créer un objet Node avec comme label Film et les propriétés adéquates
-            # A COMPLETER
+            n = Node("Film", idFilm=row[0], primaryTitle=row[1], startYear=row[2])            
             importData.append(n)
             i += 1
 
@@ -55,9 +56,32 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         except Exception as error:
             print(error)
 
-    # Names
+    # Artists
     # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tNames
-    # A COMPLETER
+    exportedCount = 0
+    cursor.execute("SELECT COUNT(1) FROM tArtist")
+    totalCount = cursor.fetchval()
+    cursor.execute("SELECT idArtist, primaryName, birthYear FROM tArtist")
+    while True:
+        importData = []
+        rows = cursor.fetchmany(BATCH_SIZE)
+        if not rows:
+            break
+
+        i = 0
+        print(rows)
+        for row in rows:
+            # Créer un objet Node avec comme label Artist et les propriétés adéquates
+            n = Node("Artist", idArtist=row[0], primaryName=row[1], birthYear=row[2])            
+            importData.append(n)
+            i += 1
+
+        try:
+            create_nodes(graph.auto(), importData, labels={"Artist"})
+            exportedCount += len(rows)
+            print(f"{exportedCount}/{totalCount} title records exported to Neo4j")
+        except Exception as error:
+            print(error)
 
     try:
         print("Indexing Film nodes...")
@@ -74,23 +98,25 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
     totalCount = cursor.fetchval()
     cursor.execute(f"SELECT idArtist, category, idFilm FROM tJob")
     while True:
-        importData = { "acted in": [], "directed": [], "produced": [], "composed": [] }
+        importData = { "acted_in": [], "directed": [], "produced": [], "composed": [] }
         rows = cursor.fetchmany(BATCH_SIZE)
         if not rows:
             break
 
         for row in rows:
             relTuple=(row[0], {}, row[2])
-            importData[row[1]].append(relTuple)
+            cat = row[1].replace(" ","_").upper()
+            importData[cat].append(relTuple)
 
         try:
             for cat in importData:
-                # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Name
+                # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Artist
                 # (les tuples nécessaires ont déjà été créés ci-dessus dans la boucle for précédente)
                 # https://py2neo.org/2021.1/bulk/index.html
                 # ATTENTION: remplacez les espaces par des _ pour nommer les types de relation
-                # A COMPLETER
-                None # Remplacez None par votre code
+                print(f"{cat} RELATIONS ENTRE LES NOEUDS")
+                for cat in importData:
+                    create_relationships(graph.auto(), importData[cat] , rel_type=cat ,  start_node_key=("Artist", "idArtist") , end_node_key=("Film", "idFilm"))
             exportedCount += len(rows)
             print(f"{exportedCount}/{totalCount} relationships exported to Neo4j")
         except Exception as error:
